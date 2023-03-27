f97a531749c644fe363c33f2d67ed7fe67c25d2f
LimbergVC <limbervillcacoraite@gmail.com>
2023-03-27 14:18:09 -0400

    test react

---
+++ frontend_react/src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+const HolaMundo = <h1>Hola Mundo</h1>
+const root =  document.getElementById('root');
+ReactDOM.render(HolaMundo, root);
