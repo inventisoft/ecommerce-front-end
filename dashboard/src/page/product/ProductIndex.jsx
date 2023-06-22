a0a162689859a61b71296f2d66826ccb7919a912
Paisa2 <villca.fuentes23@gmail.com.com>
2023-06-22 16:49:21 -0400

    modificando archivos para la coneccion

--- dashboard/src/page/product/ProductIndex.jsx
+++ dashboard/src/page/product/ProductIndex.jsx
@@ -1,9 +1,40 @@
 
 import Box from '@mui/material/Box';
 import TableProduct from '../../components/product/TableProduct';
+import { useEffect, useRef, useState } from 'react';
+import CrudServices from '../../services/crud.service';
+// import { useDispatch} from 'react-redux';
 
 
 export default function ProductIndex() {
+
+  // const dispatch = useDispatch()
+  const [products, setProducts] = useState([])
+  const [loadibg, setLoading]  = useState(true)
+  const [pagination, setPagination] = useState({})
+  const [allFilters, setAllFilters] = useState({})
+  const [showFormDialog, setShowFormDialog] = useState(false)
+  const [showDeleteDialog, setShowDeleteDialog] = useState(false)
+  const [showInfoDialog, setShowInfoDialog] = useState(false)
+  const [productSelected, setProductSelected] = useState(null)
+  const dataTableRef = useRef()
+  const formRef = useRef()
+
+  useEffect(() => {
+    const loadProducts = async (fillters={}) => {
+      setLoading(true)
+      let productList = []
+      const response = await CrudServices.getProducts(fillters)
+      if(response.status === 200) {
+        productList = response.data.data
+        setPagination(response.data.pagination)
+      }
+      setProducts(productList)
+      setLoading(false) 
+    }
+    loadProducts(allFilters)
+  }, [allFilters])
+
 
   return (
               <>
