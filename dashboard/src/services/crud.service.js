e0ffd7087fb0b1460da54c96c2c8e84822171886
Paisa2 <villca.fuentes23@gmail.com.com>
2023-06-22 16:49:21 -0400

    modificando archivos para la coneccion

--- dashboard/src/services/crud.service.js
+++ dashboard/src/services/crud.service.js
@@ -1,98 +1,98 @@
 import API from "../conection/conection"
-import authHeader from './autHeader'
+// import authHeader from './autHeader'
 
 const CrudServices = {
 
-    getUsers: async (filters = {}) => {
-        let response = {}
-        await API.get('/usuario/index', { params: filters, headers: authHeader()})
-            .then(res => response = res)
-            .catch(error => response = error.response)
-        return response
-    },
-    getUsersByRole: async (role) => {
-        let response = {}
-        await API.get('/usuario/get-users-by-role', { params:{role: role}, headers: authHeader() })
-            .then(res => response = res )
-            .catch(error => response = error.response)  
-        return response
-    },
+    // getUsers: async (filters = {}) => {
+    //     let response = {}
+    //     await API.get('/usuario/index', { params: filters, headers: authHeader()})
+    //         .then(res => response = res)
+    //         .catch(error => response = error.response)
+    //     return response
+    // },
+    // getUsersByRole: async (role) => {
+    //     let response = {}
+    //     await API.get('/usuario/get-users-by-role', { params:{role: role}, headers: authHeader() })
+    //         .then(res => response = res )
+    //         .catch(error => response = error.response)  
+    //     return response
+    // },
 
-    getUserInfo: async () => {
-        let response = {}
-        await API.get('/usuario/get-user-info', { headers: authHeader() })
-            .then(res => response = res )
-            .catch(error => response = error.response)  
-        return response
-    },
+    // getUserInfo: async () => {
+    //     let response = {}
+    //     await API.get('/usuario/get-user-info', { headers: authHeader() })
+    //         .then(res => response = res )
+    //         .catch(error => response = error.response)  
+    //     return response
+    // },
 
-    getAllUsers: async () => {
-        let response = {}
-        await API.get('/usuario/get-all-users', { headers: authHeader() })
-            .then(res => response = res )
-            .catch(error => response = error.response)  
-        return response
-    },
+    // getAllUsers: async () => {
+    //     let response = {}
+    //     await API.get('/usuario/get-all-users', { headers: authHeader() })
+    //         .then(res => response = res )
+    //         .catch(error => response = error.response)  
+    //     return response
+    // },
 
-    createUser: async (data) => {
-        let response = {}
-        await API.post('/usuario/create', data, { headers: authHeader({'Content-Type': 'multipart/form-data'}) })
-            .then(res => response = res)
-            .catch(error => response = error.response)
-        return response
-    },
+    // createUser: async (data) => {
+    //     let response = {}
+    //     await API.post('/usuario/create', data, { headers: authHeader({'Content-Type': 'multipart/form-data'}) })
+    //         .then(res => response = res)
+    //         .catch(error => response = error.response)
+    //     return response
+    // },
 
-    updateUser: async (data, id) => {
-        let response = {}
-        await API.put('/usuario/update', data, { params: {id: id}, headers: authHeader({'Content-Type': 'application/x-www-form-urlencoded'}) })
-            .then(res => response = res)
-            .catch(error => response = error.response)  
-        return response
-    },
+    // updateUser: async (data, id) => {
+    //     let response = {}
+    //     await API.put('/usuario/update', data, { params: {id: id}, headers: authHeader({'Content-Type': 'application/x-www-form-urlencoded'}) })
+    //         .then(res => response = res)
+    //         .catch(error => response = error.response)  
+    //     return response
+    // },
 
-    deleteUser: async (id) => {
-        let response = {}
-        await API.delete('/usuario/delete', { params: {id: id}, headers: authHeader() })
-            .then(res => response = res)
-            .catch(error => response = error.response)  
-        return response
-    },
+    // deleteUser: async (id) => {
+    //     let response = {}
+    //     await API.delete('/usuario/delete', { params: {id: id}, headers: authHeader() })
+    //         .then(res => response = res)
+    //         .catch(error => response = error.response)  
+    //     return response
+    // },
 
-    getUserRoles: async () => {
-        let response = {}
-        await API.get('/usuario/get-user-roles', { headers: authHeader() })
-            .then(res => response = res)
-            .catch(error => response = error.response)  
-        return response
-    },
+    // getUserRoles: async () => {
+    //     let response = {}
+    //     await API.get('/usuario/get-user-roles', { headers: authHeader() })
+    //         .then(res => response = res)
+    //         .catch(error => response = error.response)  
+    //     return response
+    // },
 
-    getRolesByAssignment: async (id) => {
-        let response = {}
-        await API.get('/usuario/get-roles-by-assignment', { params: {id:id}, headers: authHeader() })
-            .then(res => response = res)
-            .catch(error => error.response)
-        return response
-    },
+    // getRolesByAssignment: async (id) => {
+    //     let response = {}
+    //     await API.get('/usuario/get-roles-by-assignment', { params: {id:id}, headers: authHeader() })
+    //         .then(res => response = res)
+    //         .catch(error => error.response)
+    //     return response
+    // },
 
-    assignRoles: async (data) => {
-        let response = {}
-        await API.post('/usuario/assign-roles', data, { headers: authHeader({'Content-Type': 'multipart/form-data'}) })
-            .then(res => response = res)
-            .catch(error => response = error.response)
-        return response
-    },
+    // assignRoles: async (data) => {
+    //     let response = {}
+    //     await API.post('/usuario/assign-roles', data, { headers: authHeader({'Content-Type': 'multipart/form-data'}) })
+    //         .then(res => response = res)
+    //         .catch(error => response = error.response)
+    //     return response
+    // },
 
-    getProduct: async (filters= {}) => {
-        let response = {}
-        await API.get('/product/index', { params: filters, headers: authHeader() })
-        .then(res => response = res)
+    getProducts: async (filters= {}) => {
+        let response = []
+        await API.get('products', { params: filters })
+        .then(res => response = res.data)
         .catch(error => response = error.response)
         return response
     },
 
     createProduct: async (data) => {
         let response = {}
-        await API.post('/product/create', data, {headers: authHeader({'Content-Type' : 'multipart/form-data' })})
+        await API.post('/product/create', data)
             .then(res => {
                 response= res
             })
@@ -104,7 +104,7 @@ const CrudServices = {
 
     getProductById: async (id) => {
         let response = {}
-        await API.get('/product/view', { params: { id: id }, headers: authHeader() })
+        await API.get('/product/view', { params: { id: id } })
             .then(res => {
                 response = res.data
             })
@@ -116,7 +116,7 @@ const CrudServices = {
 
     updateProduct: async (data, id) => {
         let response = {}
-        await API.put('/product/update', data, { params: { id: id }, headers: authHeader({'Content-Type': 'application/x-www-form-urlencoded'})} )
+        await API.put('/product/update', data, { params: { id: id } } )
             .then(res => {
                 response =res
             }).catch(error => {
@@ -127,7 +127,7 @@ const CrudServices = {
 
     deleteProduct: async (id) => {
         let response = {}
-        await API.delete('/product/delete', {params: {id: id }, headers: authHeader()})
+        await API.delete('/product/delete', {params: {id: id }})
             .then(res => {
                 response = res
             })
